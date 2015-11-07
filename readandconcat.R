0772bf0194f60a16ea00c97c919c280892cd5533
ohgodscience <bryan@ohgodscience.com>
2015-11-07 12:28:12 -0500

    Updated r script - now returns a bunch of dataframes named after their files

--- readandconcat.R
+++ readandconcat.R
@@ -12,15 +12,24 @@ object.size(data) #Returns how large the object is (in bytes) - just to get a se
 #We might be able to merge using 'HHX' - household number.
 #In that case, we want to scrape the data into different folders for each year, loop through all the folders, and join all the files in each of the folders.
 
-dattor <- function(filename){ #This function will take a filename, and return a R object
+dattor <- function(filename, assign = T){ #This function will take a filename, and return a R object
   sasinst <- readChar(paste(filename, '.sas',sep=''), file.info(paste(filename, '.sas',sep=''))$size) #This is where the .sas input instructions are
   rdata <- read.SAScii(paste(filename,'.dat',sep=''), paste(filename,'.sas',sep=''), beginline = 1)  #This reads the file to a variable named 'data'
+  if (assign == T){
+  assign(filename, rdata, envir = .GlobalEnv)
+  }
   return(rdata)
 }
 
-datasets <- list.files(path="/", pattern="*.dat", full.names=T, recursive=FALSE)
+datasets <- list.files(path="2014", pattern="*.dat", full.names=T, recursive=FALSE)
+datasets <- sub("^([^.]*).*", "\\1", datasets)
 
-lapply(datasets, dattor) #loops through the list of files and applies the functio 'dattor', which we have above.
+lapply(datasets, dattor) #loops through the list of files and applies the functio 'dattor', which we have above
+
+#RANDOM CRAP BELOW
+
+#for all the names in 'datasets',
+#assign to a dataframe named 'dataset[i]', the corresponding processed dataframe
 
 #Is there a programmatic way to merge all the datasets?
 concatteddata <- merge(x, y, by.x = "HHX", by.y = "HHX")
